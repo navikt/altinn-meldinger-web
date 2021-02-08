c015e160b3ec76b5d4fa9f2675a6417cbdfda8f3
Mattias Lundmark <lundmark.mattias@gmail.com>
2021-02-08 08:49:19 +0100

    Boiler plate for oppsett av azuread strategy for passport

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { getConfiguredRouter } = require('./router');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const { Issuer } = require('openid-client');
+const passport = require('passport');
+const session = require('express-session');
+const { Issuer, Strategy } = require('openid-client');
 const { BASE_PATH } = require('./konstanter');
 
 
@@ -21,11 +23,38 @@ const getConfiguredAzureClient = async () => {
         client_id: 'zELcpfANLqY7Oqas',
         client_secret:
             'TQV5U29k1gHibH5bx1layBo0OSAvAbRT3UYW3EWrSYBB5swxjVfWUa1BS8lqzxG/0v9wruMcrGadany3',
-        redirect_uris: ['http://localhost:3000/altinn-meldinger-client/hei'],
+        redirect_uris: ['http://localhost:3000/oauth2/callback'],
         response_types: ['code'],
         id_token_signed_response_alg: 'RS256',
         token_endpoint_auth_method: 'client_secret_basic',
     });
+};
+
+
+const strategy = client => {
+    const verify = (tokenSet, done) => {
+        if (tokenSet.expired()) {
+            return done(null, false)
+        }
+        const user = {
+            'tokenSets': {
+                [authUtils.tokenSetSelfId]: tokenSet
+            },
+            'claims': tokenSet.claims()
+        };
+        return done(null, user);
+    };
+    const options = {
+        client: client,
+        params: {
+            response_types: ['code'],
+            response_mode: 'query',
+            scope: 'openid'
+        },
+        passReqToCallback: false,
+        usePKCE: 'S256'
+    };
+    return new Strategy(options, verify);
 };
 
 const proxy = createProxyMiddleware(FRONTEND_API_PATH, {
@@ -36,11 +65,33 @@ const proxy = createProxyMiddleware(FRONTEND_API_PATH, {
     xfwd: true,
 });
 
+const sessionOptions = {
+    cookie: {
+        maxAge: 3600000, // One hour
+        sameSite: 'lax',
+        httpOnly: true,
+    },
+    secret: 'testSecret',
+    name: 'altinn-meldinger-session-id',
+    resave: false,
+    saveUninitialized: false,
+    unset: 'destroy',
+};
+// if (process.env.NODE_ENV !== 'development') {
+//     options.cookie.secure = true;
+//     options.store = setupRedis();
+// }
+
 const startServer = async () => {
+
+    app.use(session(sessionOptions));
     const azureClient = await getConfiguredAzureClient();
     const router = getConfiguredRouter(azureClient);
 
-    app.use(proxy);
+    app.use(passport.initialize());
+    app.use(passport.session());
+    passport.use('azureOidc', strategy(azureClient));
+    // app.use(proxy);
 
     app.use('/', router);
 
