9160e53fb33c12ae4a49c116de557519bd1ac8cc
Mattias Lundmark <lundmark.mattias@gmail.com>
2021-02-10 15:51:29 +0100

    Bruker private_jwt_key som auth method for token endepunkt

--- server/server.js
+++ server/server.js
@@ -14,21 +14,22 @@ const getConfiguredAzureClient = async () => {
     const azureIssuer = await Issuer.discover(
         oauthServer + '/aad/.well-known/openid-configuration'
     );
-    return new azureIssuer.Client({
-        client_id: 'zELcpfANLqY7Oqas',
-        client_secret:
-            'TQV5U29k1gHibH5bx1layBo0OSAvAbRT3UYW3EWrSYBB5swxjVfWUa1BS8lqzxG/0v9wruMcrGadany3',
-        redirect_uris: ['http://localhost:3000/oauth2/callback'],
-        response_types: ['code'],
-        id_token_signed_response_alg: 'RS256',
-        token_endpoint_auth_method: 'client_secret_basic',
-    });
-    /*
+    // return new azureIssuer.Client({
+    //     client_id: 'audience',
+    //     client_secret:
+    //         'TQV5U29k1gHibH5bx1layBo0OSAvAbRT3UYW3EWrSYBB5swxjVfWUa1BS8lqzxG/0v9wruMcrGadany3',
+    //     redirect_uris: ['http://localhost:3000/oauth2/callback'],
+    //     response_types: ['code'],
+    //     id_token_signed_response_alg: 'RS256',
+    //     token_endpoint_auth_method: 'client_secret_basic',
+    // });
+
+
     const issuer = await Issuer.discover(oauthServer + '/aad/.well-known/openid-configuration');
     console.log(`Discovered issuer ${issuer.issuer}`);
     return new issuer.Client(
         {
-            client_id: 'CLIENT_ID',
+            client_id: 'audience',
             redirect_uris: ['http://localhost:3000/oauth2/callback'],
             token_endpoint_auth_method: 'private_key_jwt',
             token_endpoint_auth_signing_alg: 'RS256',
@@ -36,15 +37,22 @@ const getConfiguredAzureClient = async () => {
         {
             keys: [
                 {
-                    kty: 'RSA',
-                    e: 'AQAB',
-                    kid: 'mock-oauth2-server-key',
-                    n:
-                        'r4N9A6UhKVU1ntywerhKbcq2bV80iVt11ILWPzWuh1LDymVqbZBXPMtxj85EcqZn4HNhnaMN5IeSjKc36w2I5X88msscFpjqXFyutnRv1PomccVtXXzr8DFZdMbb_YTHsweAvKh4S5SYrDJEJ8Lc93m_rNBRZ8iCN14h6VlvP_McS1Vj3yDRwk6tI-4owitmkpRqs3Q_eRDty4GLWZCAm87AQcnDukDp1_U21yvNVtRzC6_QyUAF_CV2NKLcVCf5jjvyeeSvH6GAgxKkY4lyHhDr1oCqE_3RhcJ3OfcUCWJnscEydaYBoTw92ScTw1rBtFDL1DqlGA0zI5ZHCR86LQ',
-                },
-            ],
+                    "p": "36qWbr6zTCe4xM3OLpT_x9mcSbNvnx9Sr-z9GHHS4aCQS7JWirw3ez-8vWM71jypLIbUUj7ym_Dbd98IJtc9S6o8j5QejC92Y5EJqtnmKEkKnD4HWLPruCIf8mPlWFPsQQxHeAB6vv1ki036cfaHnmlV7_Fsv2ftYKHfp86-ozE",
+                    "kty": "RSA",
+                    "q": "19dG8TqsgmvM37F5C_0V0EjBXd2FcJQUkeoZbZRBcAGeoFyDklkMuUwlIogF0fQPrp-UBoCoY_efz5Y4Y_jR00W9xpPcb2spC9UdmMRVaw6YhKHY1NOdvF_9pV8hI5QtpSglaCr9o6lCp78_wELjZe7-tFcZyN7C-LvyX63W7qc",
+                    "d": "MK-8k59azRASyjD59fPySFdeo4BzHYsjObZ5Vxmblz2uD4CDKgLldQzG7ELiO4FCDhfpQklQFTpa1wIt-wFFMYW3-crTJv3gC-s3u5215yZIQ3ilMQXdXliultOQKGUnm9QHaTW0RZpgwOmOe4iRkbunrCUTPhbmiAJWzOXg1KtMAcc_q1-F5n1YpplzKLAORgRYcvfwaEKLag-FHQgqfokTp8-Lm2zWQ_2xr34ifKuQFWi79M8PMdpMzExGVoc_rDBbweXt4CxuZQTUzYp_F9D-RP4XOPl1Xb1kVXb5vW-dw3QlFHWOszwb7GK0GLHahJrs6sUHZ4I2HIHvrRPbAQ",
+                    "e": "AQAB",
+                    "use": "sig",
+                    "kid": "test-id",
+                    "qi": "heuqSTOqX0terJoTuYmudRYuldWg52KU4U-hsEFSYiOoGCuY5O-Wkqs66i28gGOlFAaU9rGvxIQd1ro9ag1TiZxyWPPtrVdyD-HwNYs11tdROTZ8lVFs6OYj3NhwZYd5TXpq4HNf-jf7yru2NF7jSWg6UR1TBXeBTHchQKdgnTo",
+                    "dp": "Wi8VUvgPoYBOrwPww7WOYM2sh8cTFczycT8UWhvjFNjB9dOls3DqygZMGuz9PofdCrgeuj7pYdk_FNlYFxkofO7aVmY53vpwOPtNM5eChvHUlmUoXyrEu8z-pqSC4BeOpjfGRWukEohnVwgNGJB35HbCkOn-mDrWauU6IhZppbE",
+                    "alg": "RS256",
+                    "dq": "hGbwg8XkUNTkBkyN3obPvMcEpxneY2LTA3dBRfDt-1FjByf5JesuXPSSyw351AMNI6eMXDjMExaxl9ukl97oh9t-QLQvQsHPmgyPbUjyxQtdD-9gXZ26YvXXWHx0jai2H1vzJmVI1f5cfx5Ycw4VBFCJOgM8M_ZCT-arFXpEIH0",
+                    "n": "vJRalsEoApXYdEFsrYo7QOcM36shQXTQvyXuiJPzqq-WpwOM7dCLUQJ7HRO07kKJAfdldwV7j1k1KELjwUozXQC_uGL28xByaP3BZ5JcTmBWz_X2tIRkxOcj8QYJYUaRBWF8gjISkbZC_Cli94NI5qkdSegGV4tjHYwGhk3qX1Pp0NULhxmfdiiMtkMj2PEGQGxl87tgUQrWjBZj1pcn-d7Tnw8uUNMt5tfqCzVXQcJefkuPTOetvFMSyMk8ismd1uxLie1sQ4I-KFnD9OMLVAHfuXNtLrQoPaYJVzqE7ix3f9y5kS8VMn8-UN42gxm6AMenJ0TXJFaAUnGAogoC9w"
+                }
+            ]
         }
-    );*/
+    );
 };
 
 const strategy = (client) => {
@@ -69,6 +77,7 @@ const strategy = (client) => {
         },
         passReqToCallback: false,
         usePKCE: 'S256',
+        sessionKey: 'altinn-meldinger-session-id'
     };
     return new Strategy(options, verify);
 };
